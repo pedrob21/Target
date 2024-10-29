dde0b443978a0f0aed7495c39f31b84536dbbdac
pedrob21 <pbernardorp@gmail.com>
2024-10-29 18:10:54 -0300

    Update main.c

--- 5/lampadas/main.c
+++ 5/lampadas/main.c
@@ -3,6 +3,6 @@
 
 int main()
 {
-    printf("Acenderia 2 lampadas e logo em seguida iria ae uma das salas. Ao voltar, desligaria um dos interruptores e voltaria na mesma sala da outra vez.\n");
+    printf("Acenderia 2 lampadas e logo em seguida iria ate uma das salas. Ao voltar, desligaria um dos interruptores e voltaria na mesma sala da outra vez.\n");
     return 0;
 }
