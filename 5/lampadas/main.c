80aed0d103afed35d7e06c7200f0c6eab7997c6e
pedrob21 <pbernardorp@gmail.com>
2024-10-29 18:09:00 -0300

    add 5

---
+++ 5/lampadas/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Acenderia 2 lampadas e logo em seguida iria ae uma das salas. Ao voltar, desligaria um dos interruptores e voltaria na mesma sala da outra vez.\n");
+    return 0;
+}
